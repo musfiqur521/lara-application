7ba30b9f229b7fea01fcbcea315149e78c27d84a
musfiqur521 <musfiqur521@gmail.com>
2023-12-11 16:55:07 +0600

    today final commit

--- resources/views/admin/category.blade.php
+++ resources/views/admin/category.blade.php
@@ -13,8 +13,7 @@
     <div class="card">
         <div class="card-header d-flex align-items-center justify-content-between">
             <h4 class="card-title">All Categories</h4>
-
-                <button class="btn btn-primary" data-toggle="modal" data-target="#AddCategoryModal">Add Category</button>
+            <button class="btn btn-primary" data-toggle="modal" data-target="#AddCategoryModal">Add Category</button>
         </div>
         <div class="card-body">
             <table class="table table-bordered" id="dataTable">
@@ -29,74 +28,74 @@
                 <tbody>
                     @foreach ($categories as $sl => $category)
                         <tr>
-                            <td>{{ ++$sl }}</td>
-                            <td>{{ $category->name }}</td>
-                            <td>{{ $category->description }}</td>
-                            <td>
+                                <td>{{ ++$sl }}</td>
+                                <td>{{ $category->name }}</td>
+                                <td>{{ $category->description }}</td>
+                                <td>
                         {{--- Unique ID Category Part ----}}
                                 <div class="d-flex">
-                                    <button class="btn btn-primary btn-sm mr-2" data-toggle="modal" data-target="{{ '#Edit' . $category->id . 'CategoryModel'}}"><i class="fas fa-edit"></i></button>
+                                        <button class="btn btn-primary btn-sm mr-2" data-toggle="modal" data-target="{{ '#Edit' . $category->id . 'CategoryModel'}}"><i class="fas fa-edit"></i></button>
 
-                                <form action="{{ route('category.destroy', $category->id) }}" method="POST" style="display: inline-block">
-                                    @csrf
-                                    @method('DELETE')
-                                <button class="btn btn-danger btn-sm" onclick="return confirm('Are you sure?')"><i class="fas fa-trash"></i></button>
-                                </form>
+                                    <form action="{{ route('category.destroy', $category->id) }}" method="POST" style="display: inline-block">
+                                        @csrf
+                                        @method('DELETE')
+                                    <button class="btn btn-danger btn-sm" onclick="return confirm('Are you sure?')"><i class="fas fa-trash"></i></button>
+                                    </form>
                                 </div>
                             </td>
                         </tr>
 
 
-       {{-- ###################  Category Edit Modal   ####################### --}}
+                        {{-- ###################  Category Edit Modal   ####################### --}}
 
-       <div class="modal fade" id="{{ 'Edit' . $category->id . 'CategoryModel'}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-       aria-hidden="true">
-       <div class="modal-dialog" role="document">
-           <div class="modal-content">
-               <div class="modal-header">
-                   <h5 class="modal-title" id="exampleModalLabel">{{ $category->name }}</h5>
-                   <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                       <span aria-hidden="true">×</span>
-                   </button>
-               </div>
+                            <div class="modal fade" id="{{ 'Edit' . $category->id . 'CategoryModel'}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+                            aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">{{ $category->name }}</h5>
+                                        <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">×</span>
+                                        </button>
+                                    </div>
 
-                 <form action="{{ route('category.update', $category->id) }}" method="POST">
-                   @csrf
-                   <input type="hidden" name="_method" value="put">
-                   <div class="modal-body">
-                       <div class="form-group">
-                           <label for="">Category Name</label>
+                                        <form action="{{ route('category.update', $category->id) }}" method="POST">
+                                        @csrf
+                                        <input type="hidden" name="_method" value="put">
+                                        <div class="modal-body">
+                                            <div class="form-group">
+                                                <label for="">Category Name</label>
 
-                           <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" value="{{ $category->name }}">
+                                                <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" value="{{ $category->name }}">
 
-                           @error('name')
-                               <span class="invalid-feedback" role="alert">
-                               <strong>{{ $message }}</strong></span>
-                           @enderror
+                                                @error('name')
+                                                    <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong></span>
+                                                @enderror
 
-                       </div>
+                                            </div>
 
-                       {{-- Description part --}}
-                       <div class="form-group">
-                           <label for="">Category Description</label>
-                           <textarea name="description" class="form-control @error('description') is-invalid @enderror" rows="5">{{ $category->description }}</textarea>
+                                            {{-- Description part --}}
+                                            <div class="form-group">
+                                                <label for="">Category Description</label>
+                                                <textarea name="description" class="form-control @error('description') is-invalid @enderror" rows="5">{{ $category->description }}</textarea>
 
-                           @error('description')
-                               <span class="invalid-feedback" role="alert">
-                               <strong>{{ $message }}</strong></span>
-                           @enderror
-                       </div>
-                   </div>
-                       <div class="modal-footer">
-                           <a class="btn btn-light" type="button" data-dismiss="modal">Cancel</a>
-                           <button class="btn btn-primary" type="submit">Update Category</button>
-                       </div>
-           </form>
-         </div>
-       </div>
-</div>
+                                                @error('description')
+                                                    <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong></span>
+                                                @enderror
+                                            </div>
+                                        </div>
+                                            <div class="modal-footer">
+                                                <a class="btn btn-light" type="button" data-dismiss="modal">Cancel</a>
+                                                <button class="btn btn-primary" type="submit">Update Category</button>
+                                            </div>
+                                </form>
+                                </div>
+                            </div>
+                        </div>
 
-                  {{-- #################################### --}}
+                    {{-- #################################### --}}
 
 
                     @endforeach
