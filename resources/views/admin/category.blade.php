2c7c3e09f483b0c9fb64ef35f1a66ba0367b70ab
musfiqur521 <musfiqur521@gmail.com>
2023-12-11 15:22:27 +0600

    category_EditDelete_MessageShow

--- resources/views/admin/category.blade.php
+++ resources/views/admin/category.blade.php
@@ -10,45 +10,44 @@
 
 @section('mainpart')
 
-    <div class="card">
-        <div class="card-header d-flex align-items-center justify-content-between">
-            <h4 class="card-title">All Categories</h4>
+<div class="card">
+    <div class="card-header d-flex align-items-center justify-content-between">
+        <h4 class="card-title">All Categories</h4>
 
-                <button class="btn btn-primary" data-toggle="modal" data-target="#AddCategoryModal">Add Category</button>
-
-        </div>
-        <div class="card-body">
-            <table class="table table-bordered" id="dataTable">
-                <thead>
-                    <tr>
-                        <th>SL</th>
-                        <th>Name</th>
-                        <th>Description</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($categories as $sl => $category)
-                        <tr>
-                            <td>{{ ++$sl }}</td>
-                            <td>{{ $category->name }}</td>
-                            <td>{{ $category->description }}</td>
-                            <td>
-                                <button class="btn btn-primary btn-sm"><i class="fas fa-edit"></i></button>
-
-                                <button class="btn btn-danger btn-sm"><i class="fas fa-trash"></i></button>
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
+            <button class="btn btn-primary" data-toggle="modal" data-target="#AddCategoryModal">Add Category</button>
     </div>
+    <div class="card-body">
+        <table class="table table-bordered" id="dataTable">
+            <thead>
+                <tr>
+                    <th>SL</th>
+                    <th>Name</th>
+                    <th>Description</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($categories as $sl => $category)
+                    <tr>
+                        <td>{{ ++$sl }}</td>
+                        <td>{{ $category->name }}</td>
+                        <td>{{ $category->description }}</td>
+                        <td>
+                            <button class="btn btn-primary btn-sm"><i class="fas fa-edit"></i></button>
+
+                            <button class="btn btn-danger btn-sm"><i class="fas fa-trash"></i></button>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
 
 
 
         <!-- Category Add Modal-->
-        <div class="modal fade" id="AddCategoryModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+<div class="modal fade" id="AddCategoryModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
         aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
@@ -64,24 +63,37 @@
                 <form action="{{ route('category.store') }}" method="POST">
                     @csrf
                     <div class="modal-body">
+                        <div class="form-group">
+                            <label for="">Category Name</label>
 
-                            <div class="form-group">
-                                <label for="">Category Name</label>
-                                <input type="text" name="name" class="form-control">
-                            </div>
-                            <div class="form-group">
-                                <label for="">Category Description</label>
-                                <textarea name="description" class="form-control" rows="5"></textarea>
-                            </div>
+                            <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" value="{{ old('name') }}">
+
+                            @error('name')
+                                <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong></span>
+                            @enderror
+
                         </div>
-                    <div class="modal-footer">
-                        <a class="btn btn-light" type="button" data-dismiss="modal">Cancel</a>
-                        <button class="btn btn-primary" type="submit">Add Category</button>
+
+                        {{-- Description part --}}
+                        <div class="form-group">
+                            <label for="">Category Description</label>
+                            <textarea name="description" class="form-control @error('name') is-invalid @enderror" rows="5">{{ old('description') }}</textarea>
+
+                            @error('description')
+                                <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong></span>
+                            @enderror
+                        </div>
                     </div>
+                        <div class="modal-footer">
+                            <a class="btn btn-light" type="button" data-dismiss="modal">Cancel</a>
+                            <button class="btn btn-primary" type="submit">Add Category</button>
+                        </div>
             </form>
 
             </div>
         </div>
-    </div>
+</div>
 
 @endsection
