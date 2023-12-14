e0109947bb27aab8d710a29cf274958483954a95
musfiqur521 <musfiqur521@gmail.com>
2023-12-14 17:49:44 +0600

    Post_comments

--- resources/views/layouts/banner.blade.php
+++ resources/views/layouts/banner.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
         <div class="col-lg-9 mx-auto">
-            <h1 class="mb-5">What Would You <br> Like To Read Today?</h1>
+            <h1 class="mb-5">Laravel Project: <br> Blog Application</h1>
             <ul class="list-inline widget-list-inline">
 
                 @foreach ($categories as $category)
